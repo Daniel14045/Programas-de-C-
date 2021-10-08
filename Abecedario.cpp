2dc44883cf59d2d8e94d41a3c7e0896bd426b788
Daniel14045 <92120767+Daniel14045@users.noreply.github.com>
2021-10-07 22:59:18 -0500

    Add files via upload

---
+++ Abecedario.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<iostream>
+using namespace std;
+int main ()
+{
+	
+
+
+char s,r,m,d;
+
+cout<<"  A"<<endl;
+cout<<"  B"<<endl;
+cout<<"  C"<<endl;
+cout<<"  D"<<endl;
+cout<<"  M"<<endl;
+cout<<"  E"<<endl;
+cout<<"  R"<<endl;
+cout<<"  O"<<endl;
+cout<<"  F"<<endl;
+cout<<"  N"<<endl;
+
+
+cout<<"##########Elige una letra para verificar sus palabras que le corresponde##########"<<endl;
+cin>>a;
+
+
+switch(a){
+	
+	
+
+	
+	case 'a': 
+	            cout<<"Area"<<endl;
+	            cout<<"Arena"<<endl;
+				cout<<"Arbol"<<endl;
+				break;
+				
+	case 'b': 
+	            cout<<"Bolsa"<<endl;
+	            cout<<"Bestia"<<endl;
+				cout<<"bicicleta"<<endl;
+				break;			  
+
+    case 'c':
+    	         cout<<"Cepillo"<<endl;
+	             cout<<"Carne"<<endl;
+				 cout<<"Conejo"<<endl;
+			    break;
+			    
+	case 'd':
+	            cout<<"Dinosaurio"<<endl;
+	            cout<<"Diente"<<endl;
+				cout<<"Diario"<<endl;
+				break;		    
+	
+	case 'm':
+		        cout<<"Mama"<<endl;
+	            cout<<"Miniatura"<<endl;
+				cout<<"Miembro"<<endl;
+				break;
+
+    case 'e':
+    	        cout<<"Elefante"<<endl;
+	            cout<<"Erizo"<<endl;
+				cout<<"Esponja"<<endl;
+				break;
+
+    case 'r':
+    	        cout<<"Rio"<<endl;
+	            cout<<"Rinoceronte"<<endl;
+				cout<<"Rana"<<endl;
+				break;
+
+    case 'o':    
+	            cout<<"Oso"<<endl;
+	            cout<<"Orca"<<endl;
+				cout<<"Oruga"<<endl;
+				break; 
+				
+	case 'f':
+	            cout<<"Filosofia"<<endl;
+	            cout<<"Fila"<<endl;
+				cout<<"Fiesta"<<endl;
+				break;
+				
+	case 'n':
+	            cout<<"Niño"<<endl;
+	            cout<<"Navidad"<<endl;
+				cout<<"Notario"<<endl;
+				break; 
+				
+}
+				defecult:
+				 cout<<"ERROR,el numero no existe"<<endl;
+				 				
+
+
+
+return 0;
+
+}
