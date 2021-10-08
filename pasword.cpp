3392d29b1b73ee9c6527dd5bb6f87272bc2fb8c1
Daniel14045 <92120767+Daniel14045@users.noreply.github.com>
2021-10-07 22:54:07 -0500

    Add files via upload

---
+++ pasword.cpp
@@ -0,0 +1,29 @@
+#include"iostream"
+#include "string"
+#include "stdlib.h" */para hacer funcionar el system("PAUSE"")
+using namespace std;
+int main(){
+	
+string  pasword;
+
+
+cout<<"Ingresa por favor tu contraseña"<<endl;
+cin>>pasword;
+
+if(pasword=="1234567"){
+	
+	
+	cout<<"Pasword correcto bienvenido "<<endl;
+}
+
+else{
+	
+	cout<<"Pasword no es correcto no eres usuario"<<endl;
+}
+
+
+
+system("PAUSE");
+
+}
+
