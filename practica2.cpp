64c40c36f7483e4e94bd8adcca171b8b05954a71
Daniel14045 <92120767+Daniel14045@users.noreply.github.com>
2021-10-12 21:38:23 -0500

    Add files via upload

---
+++ practica2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	char x[19];
+	
+	x[0]='P';
+	x[1]='r';
+	x[2]='o';
+	x[3]='g';
+	x[4]='r';
+	x[5]='a';
+	x[6]='m';
+	x[7]='a';
+	x[8]='c';
+	x[9]='i';
+	x[10]='o';
+	x[11]='n';
+	x[12]=' ';
+	x[13]='B';
+	x[14]='a';
+	x[15]='s';
+	x[16]='i';
+	x[17]='c';
+	x[18]='a';
+	
+	for(int z=0; z<19; z++){
+		
+		cout<<x[z];
+		cout<<endl;
+	}
+	
+	return 0;
+	cin.get();
+}
+	
+	
+
