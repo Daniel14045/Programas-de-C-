0c1662288435a51e6deadafaca9101908d2cbc0a
Daniel14045 <92120767+Daniel14045@users.noreply.github.com>
2021-10-12 21:39:28 -0500

    Add files via upload

---
+++ Nombre.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	char b[20];
+	
+	b[0]='D';
+	b[1]='a';
+	b[2]='n';
+	b[3]='i';
+	b[4]='e';
+	b[5]='l';
+	b[6]=' ';
+	b[7]='S';
+	b[8]='a';
+	b[9]='n';
+	b[10]='c';
+	b[11]='h';
+	b[12]='e';
+	b[13]='z';
+	b[14]='  ';
+	b[15]='R';
+	b[16]='e';
+	b[17]='y';
+	b[18]='e';
+	b[19]='s';
+	
+	
+	for(int z=0; z<22; z++){
+		
+		cout<<b[z];
+		cout<<endl;
+	}
+	
+	return 0;
+	cin.get();
+}
